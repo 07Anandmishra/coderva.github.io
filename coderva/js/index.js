43ad25ddf52c84f532e87c69b2a8cfc19f0927b9
07Anandmishra <70048617+07Anandmishra@users.noreply.github.com>
2022-08-26 09:08:09 +0530

    Add files via upload

---
+++ coderva/js/index.js
@@ -0,0 +1,41 @@
+// const navbar= document.querySelector(".navbar").querySelectorAll('.nav-link')
+
+// let ul = document.querySelector('navbar-nav')
+// console.log(ul)
+// let li = document.querySelector('navbar-item')
+// console.log(li)
+// console.log(navbar)
+
+//  li.forEach(element=>{
+//     element.addEventListener("click", function(){
+//         //navbar.forEach(nav=>nav.classList.remove("active"))
+//         // // console.log(li)
+//         // this.classList.add("active")
+//   ul.querySelector('.active').classList.remove('active')
+//   element.classList.add('active')
+//     })
+// })
+
+// li.forEach(element => {
+//     element.addEventListener('click',function(){
+//         ul.querySelector('.active').classList.remove('active')
+//         element.classList.add('actve')
+//     })
+// });
+
+// document.querySelectorAll(".nav-item").forEach((ele) =>
+//   ele.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     document
+//       .querySelectorAll(".nav-item")
+//       .forEach((ele) => ele.classList.remove("active"));
+//     this.classList.add("active")
+//   })
+// );
+
+// $(".nav-item").click(function(e) {  
+//     e.preventDefault('active')
+//     $(".nav-item").removeClass("active"); // This will remove active classes from all <li> tags
+//     $(this).addClass("active"); // This will add active class in clicked <li>   
+//   });
+
